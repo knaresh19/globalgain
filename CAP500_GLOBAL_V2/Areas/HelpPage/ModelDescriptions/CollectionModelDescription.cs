69d1f33215e985e356ac1281707b35c136c6f015
NUGRAHA Febri Ruri <dja.frnugraha@cma-cgm.com>
2021-09-19 16:31:10 +0700

    remove jenkins template folder

--- CAP500_GLOBAL_V2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace CAP500_GLOBAL_V2space.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- CAP500_GLOBAL_V2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CAP500_GLOBAL_V2space.WebApiApplication" Language="C#" %>
