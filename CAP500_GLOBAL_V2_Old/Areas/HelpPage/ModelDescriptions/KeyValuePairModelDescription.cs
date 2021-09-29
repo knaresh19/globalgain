9be37658e65c331ad60b73f76391cbcb20530d8b
NUGRAHA Febri Ruri <dja.frnugraha@cma-cgm.com>
2021-09-29 09:27:48 +0700

    remove old folder

--- CAP500_GLOBAL_V2_Old/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace CAP500_GLOBAL_V2space.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
